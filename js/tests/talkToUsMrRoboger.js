2366d95c7d6b4c3458703ebe99355466d00547a7
AlyxMoon <allisterkmoon@gmail.com>
2021-06-11 11:03:42 -0700

    add additional tests to ensure talkToUsMrRoboger works correctly

--- js/tests/talkToUsMrRoboger.js
+++ js/tests/talkToUsMrRoboger.js
@@ -10,4 +10,32 @@ const addTestsForTalkToUsMrRoboger = testManager => {
     expected: ['0'],
     args: [0],
   })
+
+  testManager.addTest({
+    description: 'It should return correctly with 1',
+    func: talkToUsMrRoboger,
+    expected: ['0', 'Beep!'],
+    args: [1],
+  })
+
+  testManager.addTest({
+    description: 'It should return correctly with 2',
+    func: talkToUsMrRoboger,
+    expected: ['0', 'Beep!', 'Boop!'],
+    args: [2],
+  })
+
+  testManager.addTest({
+    description: 'It should return correctly with 3',
+    func: talkToUsMrRoboger,
+    expected: ['0', 'Beep!', 'Boop!', 'Won\'t you be my neighbor?'],
+    args: [3],
+  })
+
+  testManager.addTest({
+    description: 'It should return correctly with 4',
+    func: talkToUsMrRoboger,
+    expected: ['0', 'Beep!', 'Boop!', 'Won\'t you be my neighbor?', '4'],
+    args: [4],
+  })
 }
