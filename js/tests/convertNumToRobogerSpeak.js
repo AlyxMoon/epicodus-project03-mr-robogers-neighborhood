95bfc1c8df1dba8a3248c8e41fd216a6cf46628a
AlyxMoon <allisterkmoon@gmail.com>
2021-06-11 09:52:37 -0700

    add rules for first nine digits, plus logic to pass the tests

--- js/tests/convertNumToRobogerSpeak.js
+++ js/tests/convertNumToRobogerSpeak.js
@@ -8,4 +8,67 @@ const addTestsForConvertNumToRobogerSpeak = testManager => {
     expected: '0',
     args: [0],
   })
+
+  testManager.addTest({
+    func: convertNumToRobogerSpeak,
+    description: 'It should turn 1 into "Beep!"',
+    expected: 'Beep!',
+    args: [1],
+  })
+
+  testManager.addTest({
+    func: convertNumToRobogerSpeak,
+    description: 'It should turn 2 into "Boop!"',
+    expected: 'Boop!',
+    args: [2],
+  })
+
+  testManager.addTest({
+    func: convertNumToRobogerSpeak,
+    description: 'It should turn 3 into "Won\'t you be my neighbor?"',
+    expected: 'Won\'t you be my neighbor?',
+    args: [3],
+  })
+
+  testManager.addTest({
+    func: convertNumToRobogerSpeak,
+    description: 'It should turn 4 into "4"',
+    expected: '4',
+    args: [4],
+  })
+
+  testManager.addTest({
+    func: convertNumToRobogerSpeak,
+    description: 'It should turn 5 into "5"',
+    expected: '5',
+    args: [5],
+  })
+
+  testManager.addTest({
+    func: convertNumToRobogerSpeak,
+    description: 'It should turn 6 into "6"',
+    expected: '6',
+    args: [6],
+  })
+
+  testManager.addTest({
+    func: convertNumToRobogerSpeak,
+    description: 'It should turn 7 into "7"',
+    expected: '7',
+    args: [7],
+  })
+
+  testManager.addTest({
+    func: convertNumToRobogerSpeak,
+    description: 'It should turn 8 into "8"',
+    expected: '8',
+    args: [8],
+  })
+
+  testManager.addTest({
+    func: convertNumToRobogerSpeak,
+    description: 'It should turn 9 into "9"',
+    expected: '9',
+    args: [9],
+  })
 }
